d66ac77c2560ad8513eacc4975051746d369b295
--global <tamizh88@gmail.com>
2015-11-04 02:30:43 +0530

    Adding Landing Page

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ var path = require('path');
 var ROOT_DIR = path.resolve(__dirname);
 var BUILD_DIR = path.resolve(ROOT_DIR, 'src/client/public');
 var APP_DIR = path.resolve(ROOT_DIR, 'src/client/app');
-
+var CSS_DIR = path.resolve(ROOT_DIR, 'src/client/css');
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: [
+    APP_DIR + '/index.jsx',
+    CSS_DIR + '/main.scss'
+  ],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
@@ -16,6 +19,11 @@ var config = {
         test : /\.jsx?/,
         include : APP_DIR,
         loader : 'babel'
+      },
+      {
+        test : /\.scss/,
+        include : CSS_DIR,
+        loader: 'style!css!sass'
       }
     ]
   }
